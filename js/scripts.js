6f277dfacb450c13ec7bca2b62e932b68085d4ff
Anna Kuznetsova and Saul Artiga <saul@>
2017-06-01 08:50:04 -0700

    add form inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+
 $(document).ready(function() {
-  
+  $("form#survey").submit(function(event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var beverage = $("#beverage").val();
+    var favoriteColor = $("#color").val();
+    var flavor = $("input:radio[name=flavor]:checked").val();
+    var result = "Your name is " + name + ", your favorite drink is " + beverage + ", your favorite song is " + flavor + ", and your favorite color is " + favoriteColor;
+    $("#output").text(result);
+  });
 });
